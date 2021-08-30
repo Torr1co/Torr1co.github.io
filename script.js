70de4647dd7a7898822b59ea4ea82d3b37109111
Torr1co <fabriziotorrico27@gmail.com>
2021-08-30 16:04:48 -0300

    update 1 small bugs

--- script.js
+++ script.js
@@ -1,81 +1,81 @@
-'use strict';
+"use strict";
 
-const modal = document.querySelector('.modal');
-const overlay = document.querySelector('.overlay');
-const btnCloseModal = document.querySelector('.btn--close-modal');
-const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
-const btnScrollTo = document.querySelector('.btn--scroll-to');
-const section1 = document.querySelector('#section--1');
-const nav = document.querySelector('.nav');
-const tabs = document.querySelectorAll('.operations__tab');
-const tabsContainer = document.querySelector('.operations__tab-container');
-const tabsContent = document.querySelectorAll('.operations__content');
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".btn--close-modal");
+const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
+const btnScrollTo = document.querySelector(".btn--scroll-to");
+const section1 = document.querySelector("#section--1");
+const nav = document.querySelector(".nav");
+const tabs = document.querySelectorAll(".operations__tab");
+const tabsContainer = document.querySelector(".operations__tab-container");
+const tabsContent = document.querySelectorAll(".operations__content");
 
 ///////////////////////////////////////
 // Contact Info
 
 const sendEmail = function (name, email, subject, message) {
   Email.send({
-    Host: 'smtp.gmail.com',
-    Username: 'fabriziotorricoacosta@gmail.com,',
-    Password: 'vvhoeaomlcshxspu',
-    To: 'fabriziotorricoacosta@gmail.com',
+    Host: "smtp.gmail.com",
+    Username: "fabriziotorricoacosta@gmail.com,",
+    Password: "vvhoeaomlcshxspu",
+    To: "fabriziotorricoacosta@gmail.com",
     From: `fabriziotorricoacosta@gmail.com`,
     Subject: `${name} sent you a message, ${subject}`,
     Body: `Name: ${name} <br/> Email:${email} <br/> Message:  ${message}`,
-  }).then(message => alert('mail sent succesfully '));
+  }).then((message) => alert("mail sent succesfully "));
 };
 
 const submitForm = function (e) {
   e.preventDefault();
 
-  let name = document.querySelector('.name').value;
-  let email = document.querySelector('.email').value;
-  let subject = document.querySelector('.subject').value;
-  let message = document.querySelector('.message').value;
+  let name = document.querySelector(".name").value;
+  let email = document.querySelector(".email").value;
+  let subject = document.querySelector(".subject").value;
+  let message = document.querySelector(".message").value;
 
-  document.querySelector('.modal__form').reset();
+  document.querySelector(".modal__form").reset();
   sendEmail(name, email, subject, message);
 };
 
-document.querySelector('.modal__form').addEventListener('submit', submitForm);
+document.querySelector(".modal__form").addEventListener("submit", submitForm);
 
 // Modal window
-document.querySelector('.modal__form').reset();
+document.querySelector(".modal__form").reset();
 const openModal = function (e) {
   e.preventDefault();
-  modal.classList.remove('hidden');
-  overlay.classList.remove('hidden');
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
 };
 
 const closeModal = function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
 };
 
-btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
+btnsOpenModal.forEach((btn) => btn.addEventListener("click", openModal));
 
-btnCloseModal.addEventListener('click', closeModal);
-overlay.addEventListener('click', closeModal);
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
 
-document.addEventListener('keydown', function (e) {
-  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
     closeModal();
   }
 });
 
 ///////////////////////////////////////
 // Button scrolling
-btnScrollTo.addEventListener('click', function (e) {
+btnScrollTo.addEventListener("click", function (e) {
   const s1coords = section1.getBoundingClientRect();
   console.log(s1coords);
 
   console.log(e.target.getBoundingClientRect());
 
-  console.log('Current scroll (X/Y)', window.pageXOffset, window.pageYOffset);
+  console.log("Current scroll (X/Y)", window.pageXOffset, window.pageYOffset);
 
   console.log(
-    'height/width viewport',
+    "height/width viewport",
     document.documentElement.clientHeight,
     document.documentElement.clientWidth
   );
@@ -92,7 +92,7 @@ btnScrollTo.addEventListener('click', function (e) {
   //   behavior: 'smooth',
   // });
 
-  section1.scrollIntoView({ behavior: 'smooth' });
+  section1.scrollIntoView({ behavior: "smooth" });
 });
 
 ///////////////////////////////////////
@@ -110,47 +110,47 @@ btnScrollTo.addEventListener('click', function (e) {
 // 1. Add event listener to common parent element
 // 2. Determine what element originated the event
 
-document.querySelector('.nav__links').addEventListener('click', function (e) {
+document.querySelector(".nav__links").addEventListener("click", function (e) {
   e.preventDefault();
 
   // Matching strategy
-  if (e.target.classList.contains('nav__link')) {
-    const id = e.target.getAttribute('href');
-    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  if (e.target.classList.contains("nav__link")) {
+    const id = e.target.getAttribute("href");
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
   }
 });
 
 ///////////////////////////////////////
 // Tabbed component
 
-tabsContainer.addEventListener('click', function (e) {
-  const clicked = e.target.closest('.operations__tab');
+tabsContainer.addEventListener("click", function (e) {
+  const clicked = e.target.closest(".operations__tab");
 
   // Guard clause
   if (!clicked) return;
 
   // Remove active classes
-  tabs.forEach(t => t.classList.remove('operations__tab--active'));
-  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+  tabs.forEach((t) => t.classList.remove("operations__tab--active"));
+  tabsContent.forEach((c) => c.classList.remove("operations__content--active"));
 
   // Activate tab
-  clicked.classList.add('operations__tab--active');
+  clicked.classList.add("operations__tab--active");
 
   // Activate content area
   document
     .querySelector(`.operations__content--${clicked.dataset.tab}`)
-    .classList.add('operations__content--active');
+    .classList.add("operations__content--active");
 });
 
 ///////////////////////////////////////
 // Menu fade animation
 const handleHover = function (e) {
-  if (e.target.classList.contains('nav__link')) {
+  if (e.target.classList.contains("nav__link")) {
     const link = e.target;
-    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
-    const logo = link.closest('.nav').querySelector('img');
+    const siblings = link.closest(".nav").querySelectorAll(".nav__link");
+    const logo = link.closest(".nav").querySelector("img");
 
-    siblings.forEach(el => {
+    siblings.forEach((el) => {
       if (el !== link) el.style.opacity = this;
     });
     logo.style.opacity = this;
@@ -158,26 +158,26 @@ const handleHover = function (e) {
 };
 
 // Passing "argument" into handler
-nav.addEventListener('mouseover', handleHover.bind(0.5));
-nav.addEventListener('mouseout', handleHover.bind(1));
+nav.addEventListener("mouseover", handleHover.bind(0.5));
+nav.addEventListener("mouseout", handleHover.bind(1));
 
 ///////////////////////////////////////
 // Sticky navigation: Intersection Observer API
 
-const header = document.querySelector('.header');
+const header = document.querySelector(".header");
 const navHeight = nav.getBoundingClientRect().height;
-const navLogo = document.querySelector('.nav__logo');
+const navLogo = document.querySelector(".nav__logo");
 
 const stickyNav = function (entries) {
   const [entry] = entries;
   // console.log(entry);
 
   if (!entry.isIntersecting) {
-    nav.classList.add('sticky');
-    navLogo.src = 'img/logo.svg';
+    nav.classList.add("sticky");
+    navLogo.src = "img/logo.png";
   } else {
-    nav.classList.remove('sticky');
-    navLogo.src = 'img/logo_white.png';
+    nav.classList.remove("sticky");
+    navLogo.src = "img/logo_white.png";
   }
 };
 
@@ -191,14 +191,14 @@ headerObserver.observe(header);
 
 ///////////////////////////////////////
 // Reveal sections
-const allSections = document.querySelectorAll('.section');
+const allSections = document.querySelectorAll(".section");
 
 const revealSection = function (entries, observer) {
   const [entry] = entries;
 
   if (!entry.isIntersecting) return;
 
-  entry.target.classList.remove('section--hidden');
+  entry.target.classList.remove("section--hidden");
   observer.unobserve(entry.target);
 };
 
@@ -209,11 +209,11 @@ const sectionObserver = new IntersectionObserver(revealSection, {
 
 allSections.forEach(function (section) {
   sectionObserver.observe(section);
-  section.classList.add('section--hidden');
+  section.classList.add("section--hidden");
 });
 
 // Lazy loading images
-const imgTargets = document.querySelectorAll('img[data-src]');
+const imgTargets = document.querySelectorAll("img[data-src]");
 
 const loadImg = function (entries, observer) {
   const [entry] = entries;
@@ -223,8 +223,8 @@ const loadImg = function (entries, observer) {
   // Replace src with data-src
   entry.target.src = entry.target.dataset.src;
 
-  entry.target.addEventListener('load', function () {
-    entry.target.classList.remove('lazy-img');
+  entry.target.addEventListener("load", function () {
+    entry.target.classList.remove("lazy-img");
   });
 
   observer.unobserve(entry.target);
@@ -233,10 +233,10 @@ const loadImg = function (entries, observer) {
 const imgObserver = new IntersectionObserver(loadImg, {
   root: null,
   threshold: 0,
-  rootMargin: '200px',
+  rootMargin: "200px",
 });
 
-imgTargets.forEach(img => imgObserver.observe(img));
+imgTargets.forEach((img) => imgObserver.observe(img));
 
 ///////////////////////////////////////
 // Slider
